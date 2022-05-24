b520062a08b7ba9337e13d3372558b419ad60174
Saleh Ahmed Mahin <salehahmedmahin@Salehs-MacBook-Air.local>
2022-05-24 14:59:20 +0600

    delete order api created

--- index.js
+++ index.js
@@ -76,6 +76,14 @@ async function run() {
       const order = await ordersCollection.find(query).toArray();
       res.send(order);
     });
+
+    // delete order
+    app.delete("/orders/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await ordersCollection.deleteOne(query);
+      res.send({ result: result });
+    });
   } finally {
   }
 }
