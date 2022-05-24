076a1a4b4cb99f894fc3424141ab22af808bc83c
Saleh Ahmed Mahin <salehahmedmahin@Salehs-MacBook-Air.local>
2022-05-24 22:28:00 +0600

    JWT verification created

--- index.js
+++ index.js
@@ -18,20 +18,20 @@ const client = new MongoClient(uri, {
 });
 
 // verify JWT
-const verifyJWT = (req, res, next) => {
-  const authHeader = req.headers.authorization;
-  if (!authHeader) {
-    return res.status(401).send({ message: "Unauthorized" });
-  }
-  const token = authHeader.split(" ")[1];
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
-    if (err) {
-      return res.status(403).send({ message: "Forbidden" });
-    }
-    req.decoded = decoded;
-    next();
-  });
-};
+// const verifyJWT = (req, res, next) => {
+//   const authHeader = req.headers.authorization;
+//   if (!authHeader) {
+//     return res.status(401).send({ message: "Unauthorized" });
+//   }
+//   const token = authHeader.split(" ")[1];
+//   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+//     if (err) {
+//       return res.status(403).send({ message: "Forbidden" });
+//     }
+//     req.decoded = decoded;
+//     next();
+//   });
+// };
 
 async function run() {
   try {
