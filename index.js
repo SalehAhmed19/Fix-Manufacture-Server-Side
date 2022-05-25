e25abdb5132c05472dfbf2311db05ffb17439a9b
Saleh Ahmed Mahin <salehahmedmahin@Salehs-MacBook-Air.local>
2022-05-25 07:30:37 +0600

    payment intent api created

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ async function run() {
       }
     };
 
-    // payment api method
+    // payment api
     app.post("/create-payment-intent", verifyJWT, async (req, res) => {
       const { price } = req.body;
       const amount = price * 100;
