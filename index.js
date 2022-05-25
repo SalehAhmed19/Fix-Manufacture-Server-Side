407b5add90fc70c9026c179b5266b760f1e270d5
Saleh Ahmed Mahin <salehahmedmahin@Salehs-MacBook-Air.local>
2022-05-26 02:46:37 +0600

    add product api created

--- index.js
+++ index.js
@@ -103,7 +103,7 @@ async function run() {
       res.send(result);
     });
 
-    // add review api method
+    // add review api
     app.post("/reviews", async (req, res) => {
       const reviews = req.body;
       const result = await reviewsCollection.insertOne(reviews);
